2b785f9f4d061d9d4ea93ee0881e30716500cfdf
anas <36207188+anesben1@users.noreply.github.com>
2023-04-22 13:43:37 -0500

    testSaturday
    - commit to solve errors

--- src/App.java
+++ src/App.java
@@ -1,6 +1,13 @@
+import java.awt.GridLayout;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -8,27 +15,32 @@ public class App {
         // create a new QuestionDatabase object by passing in the file name
         QuestionDatabase questionDB = new QuestionDatabase("data/questions.txt");
 
-        // get the list of questions from the QuestionDatabase object
+        // get the list of questions from the QuestionDatabase objectclear 
         ArrayList<Question> questions = questionDB.getQuestions();
-
-
         // get user preferences for quiz customization
-        int numQuestions = Integer.parseInt(JOptionPane.showInputDialog("How many questions would you like in the quiz?"));
-        int timeLimit = Integer.parseInt(JOptionPane.showInputDialog("How many minutes would you like for the quiz time limit?"));
-        QuizCustomization quizCustomization = new QuizCustomization(numQuestions, timeLimit);
+        JDialog quizCustomizationDialog = new JDialog();
+        quizCustomizationDialog.setLayout(new GridLayout(3, 2));
+        JLabel numQuestionsLabel = new JLabel("Number of questions:");
+        JTextField numQuestionsField = new JTextField();
+        JLabel timeLimitLabel = new JLabel("Time limit (minutes):");
+        JTextField timeLimitField = new JTextField();
+
+        // create the submit button and listener
+       
+        JButton submitButton = new JButton("Submit");
+        submitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // do something when the button is clicked
+            }
+        });
 
         QuizLogic quizLogic = new QuizLogic();
-        Quiz quiz = quizLogic.loadQuiz(quizCustomization,questionDB);
+       // Quiz quiz = quizLogic.loadQuiz(quizCustomization,questionDB);
+        //QuizUI quizUI = new QuizUI(quiz);
+        //quizUI.displayNextQuestion();
 
-        QuizUI quizUI = new QuizUI(quiz);
-        quizUI.displayNextQuestion();
-    
-
-      
-
-
-       
-
+        // close the dialog
+        quizCustomizationDialog.dispose();
         
     }
 }
